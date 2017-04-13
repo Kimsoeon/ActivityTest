45999b259b1399629574cab6578f2793e99158c7
Kimsoeon <a1thdjs@naver.com>
2017-04-13 20:50:15 +0900

    #5 Second Event Programming

--- app/src/main/java/soeonkim/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/activitytest/MainActivity.java
@@ -5,20 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit_Name, edit_Tell;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain = (Button)findViewById(R.id.butMain);
+        EditText edit_Name = (EditText)findViewById(R.id.edit_Name);
+        EditText edit_Tell = (EditText)findViewById(R.id.edit_Tell);
         butMain.setOnClickListener(butMainHandler);
     }
     View.OnClickListener butMainHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+            String name = edit_Name.getText().toString();
+            String tell = edit_Tell.getText().toString();
+            intent.putExtra("name",name);
+            intent.putExtra("tell",tell);
             startActivity(intent);
         }
     };
--- app/src/main/java/soeonkim/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/activitytest/SecondActivity.java
@@ -1,18 +1,25 @@
 package soeonkim.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name, tell;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button butSecond = (Button)findViewById(R.id.butSecond);
         butSecond.setOnClickListener(butSecondHandler);
+        Intent intent = getIntent(); //
+        name=intent.getStringExtra("name");
+        tell=intent.getStringExtra("tell");
+        TextView textResult = (TextView)findViewById(R.id.text_Result);
+        textResult.setText("* 성명 : " + name + "\n * 전화번호 : " + tell);
     }
     View.OnClickListener butSecondHandler = new View.OnClickListener() {
         @Override
